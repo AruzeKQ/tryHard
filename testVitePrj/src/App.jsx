84772ecaf625cc35dd119fabdd2ed9cc7c2eaf3f
AruzeKQ <khaiquang0103@gmail.com>
2026-05-17 02:27:24 +0700

    useHook

--- testVitePrj/src/App.jsx
+++ testVitePrj/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
-import ToDoList from "./components/todolist"
-import './App.css'
+import ProductList from "./components/useFetch"
+
 export default function App() {
     return (
         <div>
-            <ToDoList />
+            <ProductList />
         </div>
     )
 }
---
+++ testVitePrj/src/components/useFetch.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useFetcher } from './useFetch';
+
+
+export default function ProductList() {
+    const { data: info, loading, error } = useFetcher('https://api.sampleapis.com/avatar/info')
+
+    const x = JSON.stringify(data, null, 2)
+
+    if (loading) return <h2> Đang tải đợi một chút </h2>
+
+    if (error) return <h2> Lỗi rồi thằng ngu ơi =D</h2>
+
+    console.log(x)
+
+    return (
+        <div>
+            <ul>
+                <h2>Info: </h2>
+                {info && info.map((item) => {
+                    return <li key={item.id}>{item.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
